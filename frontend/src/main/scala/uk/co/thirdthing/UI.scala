ac62cfc55693b9478a4655588a6bdd6f353b7fd5
Chris Chivers <chrischivers@gmail.com>
2023-02-13 12:45:15 +0000

    fix issues with frontend decoding

--- frontend/src/main/scala/uk/co/thirdthing/UI.scala
+++ frontend/src/main/scala/uk/co/thirdthing/UI.scala
@@ -1,26 +1,22 @@
 package uk.co.thirdthing
 
-import cats.effect.IO
 import cats.syntax.all._
 import com.raquo.airstream.state.Var
-import com.raquo.laminar.api.L._
-import com.raquo.laminar.nodes.ReactiveHtmlElement
-import org.http4s.dom._
-import org.scalajs.dom.html
-import uk.co.thirdthing.model.Types.{ListingId, ListingSnapshot}
 import com.raquo.airstream.web.AjaxEventStream
 import com.raquo.airstream.web.AjaxEventStream.AjaxStreamError
 import com.raquo.laminar.api.L._
-import io.circe.{DecodingFailure, ParsingFailure}
-import org.scalajs.dom
-import io.circe.syntax._
+import com.raquo.laminar.nodes.ReactiveHtmlElement
 import io.circe.parser._
-import org.scalajs.dom.html.TableCell
-import uk.co.thirdthing.utils.TimeUtils.InstantOps
+import org.scalajs.dom
+import org.scalajs.dom.{XMLHttpRequest, html}
+import uk.co.thirdthing.model.Types.{ListingId, ListingSnapshot}
 
 import java.time.ZoneId
 
 object UI {
+
+  private val validatedListingId: Var[Option[ListingId]] = Var(initial = None)
+  private val resultsVar: Var[List[ListingSnapshot]]     = Var(initial = List.empty)
 
   private def validateUrl(url: String): Option[ListingId] = {
     val regex = "^(https://|http://)?www.rightmove.co.uk/properties/([0-9]+)".r
@@ -59,64 +55,65 @@ object UI {
       )
     }
 
-  def apply: ReactiveHtmlElement[html.Div] = {
-
-    val validatedListingId: Var[Option[ListingId]] = Var(initial = None)
-    val resultsVar: Var[List[ListingSnapshot]]     = Var(initial = List.empty)
-
-    val urlEntryElem = div(
-      label(cls := "form-label", "Enter Rightmove property link: "),
-      input(
-        cls <-- validatedListingId.signal.map(id => if (id.isDefined) "form-control is-valid" else "form-control"),
-        onMountFocus,
-        placeholder := "E.g. https://www.rightmove.co.uk/properties/xxxxxxxxx",
-        onInput.mapToValue.map(validateUrl) --> validatedListingId
-      )
+  private val urlEntryElem = div(
+    label(cls := "form-label", "Enter Rightmove property link: "),
+    input(
+      cls <-- validatedListingId.signal.map(id => if (id.isDefined) "form-control is-valid" else "form-control"),
+      onMountFocus,
+      placeholder := "E.g. https://www.rightmove.co.uk/properties/xxxxxxxxx",
+      onInput.mapToValue.map(validateUrl) --> validatedListingId
     )
+  )
 
-    def submitButton(listingId: ListingId): Div =
-      div(
-        button(
-          typ("Submit"),
-          "Submit",
-          inContext { thisNode =>
-            val $click = thisNode.events(onClick)
-            val $response = $click.flatMap { _ =>
-              thisNode.ref.remove()
-              AjaxEventStream
-                .get(
-                  url = s"api/v1/history/${listingId.value}"
-                )
-                .map(r => parse(r.responseText).flatMap(_.as[List[ListingSnapshot]]).fold(throw _, identity))
-                .recover {
-                  case err: AjaxStreamError =>
-                    dom.console.log(err.getMessage)
-                    Some(List.empty[ListingSnapshot])
+  private val buttonElem = div(
+    child <-- validatedListingId.signal.map(_.fold(div())(submitButton))
+  )
+  private val resultsElem = div(
+    child <-- resultsVar.signal.map(r => formatListingResults(r))
+  )
 
-                  case err: io.circe.Error =>
-                    dom.console.log(err.getMessage)
-                    Some(List.empty[ListingSnapshot])
-                }
-            }
-            List(
-              $response --> resultsVar.updater[List[ListingSnapshot]] { case (_, next) => next }
-            )
+  private def submitButton(listingId: ListingId): Div =
+    div(
+      button(
+        typ("Submit"),
+        "Submit",
+        inContext { thisNode =>
+          val $click = thisNode.events(onClick)
+          val $response = $click.flatMap { _ =>
+            thisNode.ref.remove()
+            makeRequest(listingId)
           }
-        )
+          List(
+            $response --> { updatedValue => resultsVar.set(updatedValue) }
+          )
+        }
       )
-
-    val buttonElem = div(
-      child <-- validatedListingId.signal.map(_.fold(div())(submitButton))
-    )
-    val resultsElem = div(
-      child <-- resultsVar.signal.map(r => formatListingResults(r))
     )
 
+  private def makeRequest(listingId: ListingId): EventStream[List[ListingSnapshot]] =
+    AjaxEventStream
+      .get(
+        url = s"api/v1/history/${listingId.value}"
+      )
+      .flatMap(parseResponse)
+      .recover {
+        case err: AjaxStreamError =>
+          dom.console.log(err.getMessage)
+          Some(List.empty[ListingSnapshot])
+
+        case err: io.circe.Error =>
+          dom.console.log(err.getMessage)
+          Some(List.empty[ListingSnapshot])
+      }
+
+  private def parseResponse(response: XMLHttpRequest): EventStream[List[ListingSnapshot]] =
+    EventStream.fromTry(parse(response.responseText).flatMap(_.hcursor.downField("records").as[List[ListingSnapshot]]).toTry)
+
+  def apply: ReactiveHtmlElement[html.Div] =
     div(
       urlEntryElem,
       buttonElem,
       resultsElem
     )
 
-  }
 }
